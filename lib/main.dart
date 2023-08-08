d46f45fdd6d12c94ebefd35dfe2dcc40e25ab586
ryan541 <ryanmwaniki60@gmail.com>
2023-08-08 17:11:42 +0300

    FCM

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/note',
+      initialRoute: '/home',
       getPages: [
         GetPage(
           name: '/home',
